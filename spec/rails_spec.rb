d550c2bcb3c6bef45b3204a784e72dbb400456e2
kellydunn <defaultstring@gmail.com>
2011-11-30 16:27:42 -0800

    [spec] Attempting to mock out rails version for rails.rb tests

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -2,7 +2,20 @@ require 'spec_helper'
 
 describe "Remotipart Rails" do
   context "assigning the INSTALL PATH based on the rails verison:" do
-    it "3.0 should be correct"
-    it "3.1 should be correct"
+    before :each do
+      module Rails
+      end
+    end
+
+    it "3.0 should be correct" do
+      Object.const_set(:Rails, stub('Rails', :version => "3.0"))
+      debugger
+      puts ::Remotipart::Rails::INSTALL_PATH
+    end
+
+    it "3.1 should be correct" do
+      Object.const_set(:Rails, stub('Rails', :version => "3.1"))
+      puts ::Remotipart::Rails::INSTALL_PATH
+    end
   end
 end
