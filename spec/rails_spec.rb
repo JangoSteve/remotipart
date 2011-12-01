6f1b6e07b6425fc4870db57040cd380b6ea9e342
kellydunn <defaultstring@gmail.com>
2011-11-30 17:03:09 -0800

    [spec] Passing in rails 3.0 and 3.1

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -2,8 +2,16 @@ require 'spec_helper'
 
 describe "Remotipart Rails" do
   context "assigning the INSTALL PATH based on the rails verison" do
+    before :each do
+      if Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR == 1
+        @path = "app/assets/javascripts"
+      else
+        @path = "public/javascripts"
+      end
+    end
+
     it "should be correct" do
-      ::Remotipart::Rails::INSTALL_PATH.should == "public/javascripts"
+      ::Remotipart::Rails::INSTALL_PATH.should == @path
     end
   end
 end
