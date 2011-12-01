c3e3237bc7d1de3d9483ba233f47b60674329ba7
kellydunn <defaultstring@gmail.com>
2011-11-30 16:05:48 -0800

    [lib] Adding in a constant for determinig the proper path to install the necessary js

--- lib/remotipart/rails.rb
+++ lib/remotipart/rails.rb
@@ -2,8 +2,10 @@ module Remotipart
   module Rails
     if ::Rails.version < "3.1"
       require 'remotipart/rails/railtie'
+      INSTALL_PATH = "public/javascripts"
     else
       require 'remotipart/rails/engine'
+      INSTALL_PATH = "app/assets/javascripts"
     end
     require 'remotipart/rails/version'
   end
