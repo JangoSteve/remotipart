fd65cb3670313dfa1018b261cf1edc30c03ec51e
kellydunn <defaultstring@gmail.com>
2011-11-30 16:17:16 -0800

    [spec] Adding more pending tests

---
+++ spec/generator_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "Remotipart Generators" do
+  it "should correctly install the javascripts in /public/javascripts for non-rails 3.1 applications"
+  it "should correctly install the javascripts in /app/assets/javascripts for rails 3.1 applications"
+end
---
+++ spec/rails_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Remotipart Rails" do
+  context "assigning the INSTALL PATH based on the rails verison:" do
+    it "3.0 should be correct"
+    it "3.1 should be correct"
+  end
+end
