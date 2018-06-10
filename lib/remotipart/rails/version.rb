62cad07b0a7392e5fc44dfb773d4bbc82e7fb1fa
Nicolas Rodriguez <nicoladmin@free.fr>
2018-06-10 21:36:32 +0200

    Add frozen_string_literal: true magic comment, fix some coding style

--- lib/remotipart/rails/version.rb
+++ lib/remotipart/rails/version.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module Remotipart
   module Rails
-    VERSION = "1.4.2"
-    IFRAMETRANSPORT_VERSION = "02.06.2013"
+    VERSION = '1.4.2'
+    IFRAMETRANSPORT_VERSION = '02.06.2013'
   end
 end
